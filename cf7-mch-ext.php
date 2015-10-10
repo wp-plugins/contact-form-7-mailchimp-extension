a3c0e2b01707289eeb75cb9bf648ab3be9e14274
rnzo <rnzo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-10 05:33:47 +0000

    Upd docs
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-mailchimp-extension/trunk@1262962 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cf7-mch-ext.php
+++ cf7-mch-ext.php
@@ -7,7 +7,7 @@ Author: Renzo Johnson
 Author URI: http://renzojohnson.com
 Text Domain: contact-form-7
 Domain Path: /languages/
-Version: 0.4.15
+Version: 0.4.16
 */
 
 /*  Copyright 2013-2015 Renzo Johnson (email: renzojohnson at gmail.com)
@@ -27,7 +27,7 @@ Version: 0.4.15
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-define( 'SPARTAN_MCE_VERSION', '0.4.15' );
+define( 'SPARTAN_MCE_VERSION', '0.4.16' );
 define( 'SPARTAN_MCE_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 define( 'SPARTAN_MCE_PLUGIN_NAME', trim( dirname( SPARTAN_MCE_PLUGIN_BASENAME ), '/' ) );
 define( 'SPARTAN_MCE_PLUGIN_DIR', untrailingslashit( dirname( __FILE__ ) ) );
@@ -35,3 +35,24 @@ define( 'SPARTAN_MCE_PLUGIN_URL', untrailingslashit( plugins_url( '', __FILE__ )
 define( 'SPARTAN_MCE_PLUGIN_MODULES_DIR', SPARTAN_MCE_PLUGIN_DIR . '/modules' );
 
 require_once( SPARTAN_MCE_PLUGIN_DIR . '/lib/mailchimp.php' );
+
+function mce_error() {
+
+  if( !file_exists(WP_PLUGIN_DIR.'/contact-form-7/wp-contact-form-7.php') ) {
+
+    $mce_error_out = '<div class="error" id="messages"><p>';
+    $mce_error_out .= __('The Contact Form 7 plugin must be installed for the <b>MailChimp Extension</b> to work. <b><a href="'.admin_url('plugin-install.php?tab=plugin-information&plugin=contact-form-7&from=plugins&TB_iframe=true&width=600&height=550').'" class="thickbox" title="Contact Form 7">Install Contact Form 7  Now.</a></b>', 'mce_error');
+    $mce_error_out .= '</p></div>';
+
+  } else if ( !class_exists( 'WPCF7') ) {
+
+    $mce_error_out = '<div class="error" id="messages"><p>';
+    $mce_error_out .= __('The Contact Form 7 is installed, but <strong>you must activate Contact Form 7</strong> below for the <b>MailChimp Extension</b> to work.','mce_error');
+    $mce_error_out .= '</p></div>';
+
+  }
+
+  echo $mce_error_out;
+
+}
+add_action('admin_notices', 'mce_error');
